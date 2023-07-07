9781186043aa906eea9152d4296c965ce0abf1ce
Walter Ugalde A <walter.ugalde@gmail.com>
2023-07-06 21:00:52 -0600

    widgets bnts

--- lib/presentations/screen/buttons/buttons_screen.dart
+++ lib/presentations/screen/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   static const String name = 'buttons_screen';
@@ -10,7 +11,47 @@ class ButtonsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Patalla de botones'),
       ),
-      body: const Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back_ios_new_outlined),
+        onPressed: () {
+          context.pop();
+        },
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: const Text('Elevate Btn')),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Elevate Disabled Btn'),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              label: const Text('Elevate Btn Icon'),
+              icon: const Icon(
+                Icons.access_alarm_outlined,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
